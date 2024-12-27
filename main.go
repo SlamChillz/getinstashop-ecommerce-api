f582e2621bf3e058cab16f15f83a0df3375d027c
slamchillz <mendyslam@gmail.com>
2024-12-27 13:41:24 +0100

    update docs with schema preference

--- main.go
+++ main.go
@@ -32,8 +32,10 @@ import (
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      https://getinstashop-ecommerce-api.onrender.com
+// @host      getinstashop-ecommerce-api.onrender.com
 // @BasePath  /api/v1
+
+// @schemes https http
 
 // @securityDefinitions.apikey BearerAuth
 // @in							header
